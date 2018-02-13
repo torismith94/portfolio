9f42078f9b8fc9394993d183c1a29d60c1ca4c8c
torismith <victoriaashleysmith@gmail.com>
2018-02-12 22:15:02 -0800

    adding the drop down for mobile

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 // creating the show/hide function jQuery Toggle
-$(document).ready(function(){
-    $("#navToggle a").click(function(e){
+$(document).ready(function() {
+    $("#navToggle a").click(function(e) {
         e.preventDefault();
 
         $("header &gt; nav").slideToggle();
         $("#logo").toggleClass("menuUp menuDown");
     })
+});
+
+// drop down for mobile 
+$("header &gt; nav &gt; ul &gt; li &gt; a").click(function(e) {
+    if($( window ).width() &lt;= "600") { if($(this).siblings().size() &gt; 0 ) {
+            e.preventDefault();
+            $(this).siblings().slideToggle("fast")
+            $(this).children(".toggle").html($(this).children(".toggle").html() == 'close' ? 'expand' : 'close');
+        }
+    }
 });

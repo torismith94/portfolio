da27d90cd652fe0666e4ce57c263a4828035f64b
torismith <victoriaashleysmith@gmail.com>
2018-02-15 19:21:25 -0800

    responsive nav bar working on all size screens

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
-// creating the show/hide function jQuery Toggle
 $(document).ready(function() {
-    $("#navToggle a").click(function(e) {
+    $("#navToggle a").click(function(e){
         e.preventDefault();
-
-        $("header &gt; nav").slideToggle();
-        $("#logo").toggleClass("menuUp menuDown");
-});
-
-//toggling depending on screenwidth 
-$(window).resize(function() {
-    if($( window ).width() > "600") {
-        $("header &gt; nav").css("display", "block");
-
-        if ($("#logo").attr('class') == "menuDown") {
-            $("#logo").toggleClass("menuUp menuDown");
+		
+        $("header > nav").slideToggle("medium");
+        $("#navToggle").toggleClass("menuUp menuDown");
+    });
+    
+    $(window).resize(function() {
+        if($( window ).width() >= "600") {
+            $("header > nav").css("display", "block");
+            
+            if($("#logo").attr('class') == "menuDown") {
+                $("#logo").toggleClass("menuUp menuDown");
+            }
         }
-    }
-    else {
-        $("header &gt; nav").css("display", "none");
-    }
-});
-
-// drop down for mobile 
-$("header &gt; nav &gt; ul &gt; li &gt; a").click(function(e) {
-    if($( window ).width() <= "600") { if($(this).siblings().size() < 0 ) {
-                e.preventDefault();
+        else {
+            $("header > nav").css("display", "none");
+        }
+    });
+    
+    $("header > nav > ul > li > a").click(function(e) {
+        if($( window ).width() <= "600") {
+            if($(this).siblings().size() > 0 ) {
                 $(this).siblings().slideToggle("fast")
-                $(this).children(".toggle").html($(this).children(".toggle").html() == 'close' ? 'expand' : 'close');
+				$(this).children(".toggle").html($(this).children(".toggle").html() == 'close' ? 'expand' : 'close');
             }
         }
     });
 });
-

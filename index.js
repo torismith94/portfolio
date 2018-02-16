6f660ed9202369461de60981b82532a37bf9190d
torismith <victoriaashleysmith@gmail.com>
2018-02-15 21:07:41 -0800

    font awesome icons

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
+// creating the show/hide function jQuery Toggle
 $(document).ready(function() {
-    $("#navToggle a").click(function(e){
+    $("#navToggle a").click(function(e) {
         e.preventDefault();
-		
+
         $("header > nav").slideToggle("medium");
-        $("#navToggle").toggleClass("menuUp menuDown");
-    });
-    
-    $(window).resize(function() {
-        if($( window ).width() >= "600") {
-            $("header > nav").css("display", "block");
-            
-            if($("#logo").attr('class') == "menuDown") {
-                $("#logo").toggleClass("menuUp menuDown");
-            }
+        $("#logo").toggleClass("menuUp menuDown");
+});
+
+//toggling depending on screenwidth 
+$(window).resize(function() {
+    if($( window ).width() > "600") {
+        $("header > nav").css("display", "block");
+
+        if ($("#logo").attr('class') == "menuDown") {
+            $("#logo").toggleClass("menuUp menuDown");
         }
-        else {
-            $("header > nav").css("display", "none");
-        }
-    });
-    
-    $("header > nav > ul > li > a").click(function(e) {
-        if($( window ).width() <= "600") {
-            if($(this).siblings().size() > 0 ) {
+    }
+    else {
+        $("header > nav").css("display", "none");
+    }
+});
+
+// drop down for mobile 
+$("header > nav > ul > li > a").click(function(e) {
+    if($( window ).width() <= "600") { if($(this).siblings().size() < 0 ) {
+                e.preventDefault();
                 $(this).siblings().slideToggle("fast")
-				$(this).children(".toggle").html($(this).children(".toggle").html() == 'close' ? 'expand' : 'close');
+                $(this).children(".toggle").html($(this).children(".toggle").html() == 'close' ? 'expand' : 'close');
             }
         }
     });
 });
+

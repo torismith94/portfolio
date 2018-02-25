46c4a610da3db052e4d058f2706ec708629ae24e
torismith <victoriaashleysmith@gmail.com>
2018-02-25 15:15:16 -0800

    new .js file for simple mobile nav

--- index.js
+++ index.js
@@ -1,34 +1,8 @@
-// creating the show/hide function jQuery Toggle
-$(document).ready(function() {
-    $("#navToggle a").click(function(e) {
-        e.preventDefault();
-
-        $("header > nav").slideToggle("medium");
-        $("#logo").toggleClass("menuUp menuDown");
-});
-
-//toggling depending on screenwidth 
-$(window).resize(function() {
-    if($( window ).width() > "600") {
-        $("header > nav").css("display", "block");
-
-        if ($("#logo").attr('class') == "menuDown") {
-            $("#logo").toggleClass("menuUp menuDown");
-        }
+function myFunction () {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className="topnav";
     }
-    else {
-        $("header > nav").css("display", "none");
-    }
-});
-
-// drop down for mobile 
-$("header > nav > ul > li > a").click(function(e) {
-    if($( window ).width() <= "600") {
-        if($(this).siblings().size() > 0 ) {
-            $(this).siblings().slideToggle("fast")
-            $(this).children(".toggle").html($(this).children(".toggle").html() == 'close' ? 'expand' : 'close');
-            }
-        }
-    });
-});
-
+}

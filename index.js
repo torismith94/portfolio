bf0fa885b70508b398a7d3a5d9fedd59e1221a6c
torismith <victoriaashleysmith@gmail.com>
2018-03-04 21:00:55 -0800

    nav sticky, just need to fix padding issue on mobile for header

--- index.js
+++ index.js
@@ -7,25 +7,6 @@ function myFunction () {
     }
 }
 
-//when the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction ()};
-
-//get the navbar
-var navbar = document.getElementById("myTopnav");
-
-//get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-//adding the sticky class to the navbar when you reach its scroll position
-//remove sticky when you leave the scroll position
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
-    }
-}
-
 // //init controller
 // var controller = new ScrollMagic.Controller();
 
